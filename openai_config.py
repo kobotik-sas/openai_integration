75ccbb02f34660afa09567011f3fc5e4bd8d6f38
kobotik-sas <129522997+kobotik-sas@users.noreply.github.com>
2023-04-26 13:47:00 +0200

    Delete openai_config.py

--- openai_config.py
+++
@@ -1,9 +0,0 @@
-# openai_integration/models/openai_config.py
-
-from odoo import models, fields
-
-class OpenAIConfig(models.Model):
-    _name = 'openai.config'
-    _description = 'OpenAI Configuration'
-
-    api_key = fields.Char(string='API Key', required=True)
