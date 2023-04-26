f57a32ff2e5ce053a13bfaeaa3fdf7717b0b4844
kobotik-sas <129522997+kobotik-sas@users.noreply.github.com>
2023-04-26 13:40:33 +0200

    Create openai_integration.py

---
+++ openai_integration.py
@@ -0,0 +1,22 @@
+# openai_integration/models/openai_integration.py
+
+import requests
+from odoo import models, api
+
+class OpenAIIntegration(models.Model):
+    _name = 'openai.integration'
+    _description = 'OpenAI Integration'
+
+    @api.model
+    def get_openai_response(self, text):
+        config = self.env['openai.config'].search([], limit=1)
+        if not config:
+            raise ValueError("OpenAI API Key not configured")
+
+        headers = {'Authorization': f'Bearer {config.api_key}'}
+        data = {
+            'prompt': text,
+            'max_tokens': 100,
+        }
+
+        response = requests.post('https://api.openai.com/v1/engines/davinci-codex
