279822580f7f5ea015184c27bb95695137a03f1c
kobotik-sas <129522997+kobotik-sas@users.noreply.github.com>
2023-04-26 13:43:01 +0200

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,5 +1,22 @@
+# -*- coding: utf-8 -*-
 {
-    ...
-    'external_dependencies': {'python': ['requests']},
-    ...
+    'name': 'OpenAI Integration',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Integrate OpenAI API with Odoo',
+    'author': 'Your Name',
+    'website': 'https://www.example.com',
+    'license': 'AGPL-3',
+    'depends': [
+        # Add any Odoo module dependencies here
+    ],
+    'external_dependencies': {
+        'python': ['requests'],
+    },
+    'data': [
+        'views/openai_config_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
 }
